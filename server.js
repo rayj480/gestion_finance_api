b906f69e19285a9810d344f2c4eb343986767384
rayj480 <cari.jonathan@gmail.com>
2017-11-19 10:22:15 +0100

    mongo db integration

--- server.js
+++ server.js
@@ -1,69 +1,27 @@
 const express = require("express");
-const fs = require("fs");
-const sqlite = require("sql.js");
+var MongoClient = require('mongodb').MongoClient
+, assert = require('assert');
 
-const filebuffer = fs.readFileSync("db/usda-nnd.sqlite3");
-
-const db = new sqlite.Database(filebuffer);
 
 const app = express();
 
 app.set("port", process.env.PORT || 3001);
 
-// Express only serves static assets in production
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
-
-const COLUMNS = [
-  "carbohydrate_g",
-  "protein_g",
-  "fa_sat_g",
-  "fa_mono_g",
-  "fa_poly_g",
-  "kcal",
-  "description"
-];
 app.get("/api/food", (req, res) => {
-  const param = req.query.q;
 
-  if (!param) {
-    res.json({
-      error: "Missing required parameter `q`"
+  var url = 'mongodb://localhost:27017/gestion_finance';
+  MongoClient.connect(url, function(err, db) {
+    assert.equal(null, err);
+    console.log("Connected correctly to server");
+  
+    var col = db.collection('operations');
+    // Insert a single document
+    col.find().toArray(function(err, r) {
+      assert.equal(null, err);
+      
+      res.json(r);
     });
-    return;
-  }
-
-  // WARNING: Not for production use! The following statement
-  // is not protected against SQL injections.
-  const r = db.exec(
-    `
-    select ${COLUMNS.join(", ")} from entries
-    where description like '%${param}%'
-    limit 100
-  `
-  );
-
-  if (r[0]) {
-    res.json(
-      r[0].values.map(entry => {
-        const e = {};
-        COLUMNS.forEach((c, idx) => {
-          // combine fat columns
-          if (c.match(/^fa_/)) {
-            e.fat_g = e.fat_g || 0.0;
-            e.fat_g = (parseFloat(e.fat_g, 10) +
-              parseFloat(entry[idx], 10)).toFixed(2);
-          } else {
-            e[c] = entry[idx];
-          }
-        });
-        return e;
-      })
-    );
-  } else {
-    res.json([]);
-  }
+  });
 });
 
 app.listen(app.get("port"), () => {
